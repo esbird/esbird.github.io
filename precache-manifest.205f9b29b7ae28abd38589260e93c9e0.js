be043d70131a85f2d9146af652fc1c51376ce219
esbird <1206066291@qq.com>
2019-09-04 22:16:13 +0800

    init

---
+++ precache-manifest.205f9b29b7ae28abd38589260e93c9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5980c2b017891d401f271561c774aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36786b5eff3b8528042d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "051dccb25eef1117ff66",
+    "url": "/static/js/2.2a865702.chunk.js"
+  },
+  {
+    "revision": "36786b5eff3b8528042d",
+    "url": "/static/js/main.cd422ccf.chunk.js"
+  },
+  {
+    "revision": "590b1f1a44e44eae64fb",
+    "url": "/static/js/runtime~main.8f66630e.js"
+  }
+]);
